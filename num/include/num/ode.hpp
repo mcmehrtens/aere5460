08556f37d5422988383587bca9d534b486621861
Matthew Mehrtens <12023414+mcmehrtens@users.noreply.github.com>
2026-09-07 19:07:58 -0500

    document ode.hpp

--- num/include/num/ode.hpp
+++ num/include/num/ode.hpp
@@ -7,8 +7,17 @@
 
 namespace num {
 
+/// A fixed-size vector of `Dim` doubles representing the ODE state.
 template <std::size_t Dim> using State = std::array<double, Dim>;
 
+/// Compute ax + y where x and y have size Dim. Useful for performing operations
+/// over all state variables.
+///
+/// @tparam Dim The number of state variables.
+/// @param  a   The scalar multiplier.
+/// @param  x   The x state variables.
+/// @param  y   The y state variables.
+/// @return     The result of ax + y.
 template <std::size_t Dim>
 State<Dim> axpy(double a, const State<Dim> &x, const State<Dim> &y) {
   State<Dim> result{};
@@ -17,6 +26,17 @@ State<Dim> axpy(double a, const State<Dim> &x, const State<Dim> &y) {
   return result;
 }
 
+/// Integrate a single ODE step with 2nd-order Runge-Kutta (midpoint variant).
+/// This method requires two evaluations of RHS per step.
+///
+/// @tparam Dim The number of state variables.
+/// @tparam RHS The RHS function with expected signature `State<Dim> RHS(double
+///             t, const State<Dim> &u)`.
+/// @param  rhs The right-hand-side callable evaluating du/dt.
+/// @param  t   The current simulation time.
+/// @param  u   The current state variables.
+/// @param  dt  The size of step to integrate over.
+/// @return     The computed state variables at time t = t + dt.
 template <std::size_t Dim, typename RHS>
 State<Dim> rk2_step(RHS rhs, double t, const State<Dim> &u, double dt) {
   const double dt_half = 0.5 * dt;
@@ -24,11 +44,35 @@ State<Dim> rk2_step(RHS rhs, double t, const State<Dim> &u, double dt) {
   return axpy(dt, rhs(t + dt_half, u_mid), u);
 }
 
+/// The solution data structure. `t` and `u` are parallel vectors of the same
+/// length.
+///
+/// @tparam Dim The number of state variables.
 template <std::size_t Dim> struct Solution {
-  std::vector<double> t;
-  std::vector<State<Dim>> u;
+  std::vector<double> t;     ///< The time vector.
+  std::vector<State<Dim>> u; ///< The state variable vector.
 };
 
+/// Integrate `Dim` ODEs with RHS `RHS` from time `t0` to time `t1` with
+/// `n_points - 1` steps of stepper `Stepper` and initial conditions `u0`. dt
+/// for each step is computed as `(t1 - t0) / (n_points - 1)`.
+///
+/// @tparam Dim       The number of state variables.
+/// @tparam Stepper   The stepping function with expected signature `State<Dim>
+///                   Stepper(RHS rhs, double t, const State<Dim> &u, double
+///                   dt)`.
+/// @tparam RHS       The RHS function with expected signature `State<Dim>
+///                   RHS(double t, const State<Dim> &u)`.
+/// @param  step      The stepper algorithm callable evaluating u(t + dt) from
+///                   u(t).
+/// @param  rhs       The right-hand-side callable evaluating du/dt.
+/// @param  u0        The initial conditions, i.e., u(t0).
+/// @param  t0        The initial simulation time.
+/// @param  t1        The final simulation time.
+/// @param  n_points  The number of time points, including `t0` and `t1`; at
+///                   least 2.
+/// @return           The parallel solution vectors for the time `t` and the
+///                   state variables `u` at each time `t`.
 template <std::size_t Dim, typename Stepper, typename RHS>
 Solution<Dim> integrate(Stepper step, RHS rhs, const State<Dim> &u0, double t0,
                         double t1, std::size_t n_points) {
